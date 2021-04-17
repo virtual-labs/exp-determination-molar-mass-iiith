a6e0867be8858bf8d986cae8910efec6a99345da
Ar9020th <arthraj9@gmail.com>
2021-04-17 18:18:17 +0530

    props done

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,16 @@
 //Your JavaScript goes in here
+let startAnimation = async () => {
+  const line = document.getElementById("half_grad");
+  const yFinalPosition = 0;
+  let yPos = 100;
+  const interval = window.setInterval(() => {
+    if (yPos < yFinalPosition) {
+      line.setAttribute("y1", "0.1%");
+      return window.clearInterval(interval);
+    }
+    yPos -= 0.1;
+    line.setAttribute("y1", `${yPos}%`);
+  }, 1);
+};
+
+startAnimation();
