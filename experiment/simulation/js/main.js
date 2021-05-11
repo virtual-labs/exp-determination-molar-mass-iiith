52a9131a3f41c0d9fa6d813204915a44c7d0f715
PanwarJayant <jayant.panwar@research.iiit.ac.in>
2021-05-11 20:53:29 +0530

    Responsive update 1

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -68,7 +68,7 @@ function pur() {
       // duration: 800,
       translateY: "-25%",
       update: function (anim) {
-        // message = messages[1];
+        // message = instructionMessages[1];
         document.getElementById("spoonmouth").style.fill = "#b83dba";
         document.getElementById("spoonmouth").style.opacity = "1";
         // console.log(document.getElementById("spoonmouth"));
@@ -77,7 +77,7 @@ function pur() {
     })
     .add({
       duration: 800,
-      translateX: "480%",
+      translateX: "450%",
       translateY: "200%",
     })
     .add({
@@ -86,7 +86,7 @@ function pur() {
     })
     .add({
       update: function (anim) {
-        // message = messages[1];
+        // message = instructionMessages[1];
         document.getElementById("spoonmouth").style.fill = "#b83dba";
         document.getElementById("spoonmouth").style.opacity = "0";
         document.getElementById("pinkbottom").style.fill = "#b83dba";
@@ -98,7 +98,7 @@ function pur() {
 async function liftPiston() {
   var image = document.getElementById("syringepiston");
   // console.log(image);
-  image.style.transform = "translate(200%, -5%);";
+  image.style.transform = "translate(100%, -5%);";
   image.style.pointerEvents = "none";
   var a1 = anime.timeline({
     targets: "#syringepiston",
@@ -167,10 +167,29 @@ async function moveSyringe() {
     duration: 800,
     easing: "linear",
   });
+  let startX = "-530%";
+  let startY = "-550%";
+  let endX = "800%";
+  let endY = "-235%";
+
+  if (screen.width < 577) {
+    startX = "-175%";
+    startY = "-630%";
+    endX = "-350%";
+    endY = "80%";
+  }
+
+  if (screen.width < 400) {
+    startX = "-400%";
+    startY = "-1000%";
+    endX = "-550%";
+    endY = "125%";
+  }
+
   a1.add({
     duration: 0,
-    translateY: "-400%",
-    translateX: "-410%",
+    translateY: startY,
+    translateX: startX,
   });
   liftPiston();
   fillSyringe(1);
@@ -180,8 +199,8 @@ async function moveSyringe() {
     rotateZ: 90,
   }).add({
     duration: 1000,
-    translateY: "-175%",
-    translateX: "700%",
+    translateY: endY,
+    translateX: endX,
   });
 }
 async function shakeBeaker() {
@@ -222,10 +241,30 @@ async function moveSyringe2() {
     duration: 800,
     easing: "linear",
   });
+
+  let startX = "-985%";
+  let startY = "-150%";
+  let endX = "800%";
+  let endY = "-235%";
+
+  if (screen.width < 577) {
+    startX = "-520%";
+    startY = "-350%";
+    endX = "-350%";
+    endY = "80%";
+  }
+
+  if (screen.width < 400) {
+    startX = "-1000%";
+    startY = "-550%";
+    endX = "-550%";
+    endY = "125%";
+  }
+
   a1.add({
     duration: 0,
-    translateY: "-120%",
-    translateX: "-760%",
+    translateY: startY,
+    translateX: startX,
     rotateZ: 0,
   });
   liftPiston();
@@ -236,29 +275,30 @@ async function moveSyringe2() {
     rotateZ: 90,
   }).add({
     duration: 1000,
-    translateY: "-175%",
-    translateX: "700%",
+    translateY: endY,
+    translateX: endX,
   });
   document.getElementById("AnimationBottomRight").play();
+  document.getElementById("instructions").innerHTML = "Observations:";
   await new Promise((r) => setTimeout(r, 2000));
   iter2 += 1;
-  document.getElementById("animtext").innerHTML = animationmessages[iter2];
+  document.getElementById("instruction").innerHTML = observationMessages[iter2];
   await new Promise((r) => setTimeout(r, 5000));
   iter2 += 1;
-  document.getElementById("animtext").innerHTML = animationmessages[iter2];
+  document.getElementById("instruction").innerHTML = observationMessages[iter2];
   await new Promise((r) => setTimeout(r, 3000));
   iter2 += 1;
-  document.getElementById("animtext").innerHTML = animationmessages[iter2];
+  document.getElementById("instruction").innerHTML = observationMessages[iter2];
   await new Promise((r) => setTimeout(r, 5000));
   iter2 += 1;
-  document.getElementById("animtext").innerHTML = animationmessages[iter2];
+  document.getElementById("instruction").innerHTML = observationMessages[iter2];
   await new Promise((r) => setTimeout(r, 3000));
   iter2 += 1;
-  document.getElementById("animtext").innerHTML = animationmessages[iter2];
+  document.getElementById("instruction").innerHTML = observationMessages[iter2];
   startAnimation();
 }
-var messages = [
-  "Click on the solvent bottle to draw 1 ml of the solvent (methanol) and inject it into the sample inlet to clean any impurity in the sample pathway.",
+var instructionMessages = [
+  "Click on the solvent bottle to draw 1 ml of the solvent (methanol) and inject it into the sample inlet to clean any impurity in the sample pathway of the mass spectrometer.",
   "Click on the sample bottle to transfer small amount (around 1mg) of the substance into the empty solution bottle",
   "Click on the solvent bottle to transfer 5 ml of the solvent (methanol) to the substance taken.",
   "Click on the  solution bottle to make a clear solution.",
@@ -267,17 +307,17 @@ var messages = [
 var iter1 = -1;
 function changeMessage() {
   iter1++;
-  document.getElementById("instruction").innerHTML = messages[iter1];
+  document.getElementById("instruction").innerHTML = instructionMessages[iter1];
 }
 function removeDivs() {
   changeMessage();
   $("div").remove(".custom-control,.custom-radio");
 }
 var iter2 = -1;
-var animationmessages = [
-  "The sample is introduced into the vaporisation chamber which is instantly vapourised due to high vacuum and heat.",
+var observationMessages = [
+  "Now observe the zoomed in animation of mass spectromter. The sample is introduced into the vaporisation chamber which is instantly vapourised due to high vacuum and heat.",
   "Positively charged radical ions are formed by bombardment of beam of high energy electrons.",
   "The positively charged radical ions are accelerated by perforated negative electrodes",
   "The ions are sorted and separated by the magnetic field according to their mass/charge ratio.",
-  "Now observe the graph on the left. These lines demonstrate the molar mass of the compound in the solution Bottle.",
+  "Now observe the graph being plotted. These lines demonstrate the molar mass of the compound in the solution Bottle.",
 ];
